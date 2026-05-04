fa7a667b5f401857c6f3c645375273abf1a275f8
=MinhLe <minhle@valofe.com>
2026-05-04 15:35:24 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,7 @@ Route::get('/spot-trading', [TraddingController::class, 'spot'])->name('spot.tra
 Route::get('/demo', [DemoController::class, 'index'])->name('demo');
 Route::get('/signin', [ClientLoginController::class, 'index'])->name('signin');
 Route::get('/signup', [ClientRegisterController::class, 'index'])->name('signup');
-Route::get('/', [DashboardController::class, 'index'])->name('landing_page');
-Route::view('/landing2', 'pages.landing2')->name('landing2');
+Route::view('/', 'pages.landing2')->name('landing2');
 
 Route::get('/lang/{locale}', function (string $locale) {
     if (in_array($locale, ['en', 'vi'], true)) {
