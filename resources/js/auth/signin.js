8a7925a9c076b941a26c97a15fa9aa74f2229dda
=MinhLe <minhle@valofe.com>
2026-05-05 11:26:03 +0700

    update

--- resources/js/auth/signin.js
+++ resources/js/auth/signin.js
@@ -16,7 +16,8 @@ form.addEventListener('submit', async (e) => {
     password: form.password.value,
     remember:form.remember.checked ? 1 : 0,
   };
-
+  console.log('Submitting login form with payload:', payload);
+  console.log('CSRF Token:', getCsrfToken());
   try {
     const res = await fetch(form.action, {
       method: 'POST',
