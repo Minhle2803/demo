920e0b22c9d6190806d8d8936506b1207207252c
minhle <minhle@valofe.com>
2026-05-11 22:06:07 +0700

    update

--- app/Http/Controllers/Admin/AdminUserController.php
+++ app/Http/Controllers/Admin/AdminUserController.php
@@ -85,7 +85,7 @@ class AdminUserController extends Controller
     {
         $user = ClientUser::findOrFail($id);
 
-        $user->forceFill(['kyc_verified_at' => now()])->save();
+        $user->forceFill(['kyc_verified_at' => now(), 'is_verified' => true])->save();
 
         return redirect()->back()
             ->with('success', __('admin.kyc_approved'));
